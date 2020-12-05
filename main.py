8c4e359fbfb683a3c191bafd51da33b2605beac1
kyleecodes <kfieldsinmd@gmail.com>
2020-12-04 23:51:36 -0500

    Add comments

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-import spotipy
-import configparser
-import json
-import requests
 from spotipy import oauth2
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy
+import requests
+from secrets import client_id, client_secret
 
-config = configparser.ConfigParser()
-config.read('config.cfg')
-client_id = config.get('SPOTIFY', 'CLIENT_ID')
-client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
 
+# AUTHORIZATION
 auth = oauth2.SpotifyClientCredentials(
     client_id=client_id,
     client_secret=client_secret
@@ -20,31 +14,37 @@ auth = oauth2.SpotifyClientCredentials(
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
-# POST
+
+# POST REQUEST
 auth_response = requests.post(AUTH_URL, {
     'grant_type': 'client_credentials',
     'client_id': client_id,
     'client_secret': client_secret,
 })
 
-# convert the response to JSON
+
+# CONVERT RESPONSE TO JSON
 auth_response_data = auth_response.json()
 
-# save the access token
+
+# SAVE ACCESS TOKEN
 access_token = auth_response_data['access_token']
 
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
-# base URL of all Spotify API endpoints
+
+# GET REQUEST
 BASE_URL = 'https://api.spotify.com/v1/'
 
-# track_id = '6y0igZArWVi6Iz0rj35c1Y'
-#
-# r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
-#
-# r = r.json()
+track_id = '6y0igZArWVi6Iz0rj35c1Y'
+
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+
+r = r.json()
+
+# PRINT TO SEE THE TRACK INFO
 # print(r)
 
 artist_id = "1a4N2lwra7WGjwCDJS1Dkk"
@@ -54,26 +54,27 @@ r = requests.get(BASE_URL + 'artists/' + artist_id + '/albums',
                  params={'include_groups': 'album', 'limit': 50})
 d = r.json()
 
-data = []  # will hold all track info
-albums = []  # to keep track of duplicates
+data = []  # HOLDS TRACK INFO
+albums = []  # HOLDS ALBUMS (NO DUPLICATES)
 
-# loop over albums and get all tracks
+
+# LOOP OVER ALBUMS FOR TRACKS
 for album in d['items']:
     album_name = album['name']
     print(album_name)
 
-    # pull all tracks from this album
+    # GET TRACKS FROM ALBUM
     r = requests.get(BASE_URL + 'albums/' + album['id'] + '/tracks',
                      headers=headers)
     tracks = r.json()['items']
 
     for track in tracks:
-        # get audio features (key, liveness, danceability, ...)
+        # GET AUDIO FEATURES (KEY, DANCEABILITY, ETC.)
         f = requests.get(BASE_URL + 'audio-features/' + track['id'],
                          headers=headers)
         f = f.json()
 
-        # combine with album info
+        # COLLECT ALL ALBUM INFO
         f.update({
             'track_name': track['name'],
             'album_name': album_name,
@@ -83,20 +84,25 @@ for album in d['items']:
 
         data.append(f)
 
+
+# NOW TIME FOR DATAFRAMES
 df = pd.DataFrame(data)
-# convert release_date to an actual date, and sort by it
+# CONVERT RELEASE DATE TO ACTUAL DATE AND SORT
 df['release_date'] = pd.to_datetime(df['release_date'])
 df = df.sort_values(by='release_date')
 df.head()
 
 plt.figure(figsize=(10, 10))
 
+
+# BUILD SCATTER PLOT
 ax = sns.scatterplot(data=df, x='instrumentalness', y='speechiness',
                      hue='album_name', palette='rainbow',
                      size='duration_ms', sizes=(50, 1000),
                      alpha=0.7)
 
-# display legend without `size` attribute
+
+# DISPLAY LEGEND WITH NO SIZE ATTRIBUTE
 h, labs = ax.get_legend_handles_labels()
 ax.legend(h[1:10], labs[1:10], loc='best', title=None)
 ax.legend()
